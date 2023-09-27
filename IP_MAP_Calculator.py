732d923930495465dc90f75e01fe6fc94df85fde
sfreemire <35846458+sfreemire@users.noreply.github.com>
2023-09-27 15:10:10 -0500

    moved 'About' text to separate file

--- IP_MAP_Calculator.py
+++ IP_MAP_Calculator.py
@@ -956,38 +956,9 @@ while True:
       break
 
    if event == 'About':
-      text = '''IP MAP Calculator
-
-License:
-
-MIT License
-
-Copyright (c) 2023 David Scott Freemire
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-
-Program graphics make use of the PySimpleGUI library which is shared under the
-GNU Lesser General Public License (LGPL 3) +
-https://www.pysimplegui.org/
-'''
-      about = sg.popup_scrolled(text, title='About', font=('Aril', 14))
+      with open('applicensetext') as lt:
+         abouttxt = lt.read()
+      about = sg.popup_scrolled(abouttxt, title='About', font=('Aril', 14))
 
 ## Prints all available element keys
 #   print('\n ---- VALUES KEYS ----')
